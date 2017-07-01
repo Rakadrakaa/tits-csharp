6c06ccbf24df572da6f69f7ce7bf7365bf7e7654
Denis Rodionov <rodionof.denis@yandex.ru>
2017-07-02 00:48:49 +0300

    final
    eee

---
+++ tits.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace tits_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----0-----------0--0-----------0----");
+            Console.WriteLine("---0-------------00-------------0---");
+            Console.WriteLine("---0------+------00------+------0---");
+            Console.WriteLine("---0----+++++----00----+++++----0---");
+            Console.WriteLine("----0-----+-----0--0-----+-----0----");
+            Console.WriteLine("-----00-------00----00-------00-----");
+            Console.WriteLine("-------0000000--------0000000-------");
+            Console.ReadLine();
+        }
+    }
+}
